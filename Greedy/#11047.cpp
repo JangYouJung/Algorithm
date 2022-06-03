6d14815fe5d1035193e346fed7c4d4e382eda9a9
YueJeong <80906691+YueJeong@users.noreply.github.com>
2022-06-03 15:48:07 +0900

    Solved #11047

---
+++ Greedy/#11047.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> coin;
+    int value;
+    int answer = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> value;
+        coin.push_back(value);
+    }
+
+    //Greedy Algorithm
+    for (int i = n - 1; i > -1; i--) {
+        while (k >= coin[i]) {           
+            k -= coin[i];
+            answer++;
+            if (k == 0) {
+                cout << answer;
+                break;
+            }
+        }        
+    }
+    
+    return 0;
+}
